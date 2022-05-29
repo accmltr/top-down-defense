86bd3e5565586ddcb2457cbbe55e0c562ed1852e
VastView <johanreynekemail@gmail.com>
2022-05-29 17:31:56 +1200

    Work on navpoly utility.

--- Top-down Defense (Godot Project)/Scripts/Pathfinder.gd
+++ Top-down Defense (Godot Project)/Scripts/Pathfinder.gd
@@ -14,9 +14,8 @@ func _ready():
 
 
 
-func organizeIntoIslands(merge_distance: float = 40):
+func organizeIntoIslands(merge_distance: float = 0):
 	# Takes the circle shapes from turrets/walls and organizes them into groups that should get merged together with filled spaces.
-	var mdSqrd = pow(merge_distance, 2)
 	var islands = [] # Array of PoolVector3Arrays with elemnts of type (x_pos, y_pos, radius)
 
 	for col in circle_colliders.get_children():
@@ -27,20 +26,44 @@ func organizeIntoIslands(merge_distance: float = 40):
 		var belong_to = [] # record which islands element belongs to by saving island index
 		for i in islands.size():
 			for circle in i:
-				if pos.distance_squared_to(Vector2(circle.x, circle.y)) - (r+circle.y) < mdSqrd:
+				if pos.distance_to(Vector2(circle.x, circle.y)) - (r+circle.y) < merge_distance:
 					belong_to.append(i)
 					break
+		
+		# Merge or create islands as neccesary:
 		if belong_to.size() == 0:
 			islands.append(PoolVector3Array([element]))
 		else:
-			# Add element to first island index in "belong_to":
 			islands[belong_to[0]].append(element)
-			# Merge all islands in "belong_to" in "islands" into one island, and modify "islands" accordingly:
 			
+			var m = PoolVector3Array() # merged island
+			
+			for i in belong_to:
+				for e in islands[i]:
+					m.append(e)
+			
+			var new_islands = [m]
+			
+			for i in islands.size():
+				if !belong_to.has(i):
+					new_islands.append(islands[i])
+			
+			islands = new_islands
+		
+		# Go through every island and create them:
 
+# UNFINISHED:
+func mergeCircles(merge_distance: float, c1: Vector3, c2: Vector3) -> PoolVector2Array:
+	var p1 = shiftPolygon(makeCirclePolygon(c1.y), Vector2(c1.x, c1.y))
+	var p2 = shiftPolygon(makeCirclePolygon(c2.y), Vector2(c2.x, c2.y))
+	
+	return Geometry.merge_polygons_2d(p1, p2)[0] as PoolVector2Array
 
-
-
+func shiftPolygon(poly: PoolVector2Array, offset: Vector2):
+	var res = PoolVector2Array()
+	for pt in poly:
+		res.append(pt + offset)
+	return res
 
 
 func makeCirclePolygon(radius: float, vertices: float = 16) -> PoolVector2Array:
